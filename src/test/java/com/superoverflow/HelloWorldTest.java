e4e6974c894a95014e67cd5ae813b4a068e47da7
Cyrus Tang <cyrus.tang@superoverflow.com>
2022-10-02 16:50:26 +0100

    Gradle HelloWorld

---
+++ src/test/java/com/superoverflow/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package com.superoverflow;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void verifyNoExceptionThrown() {
+        HelloWorld.main(new String[]{});
+    }
+}
